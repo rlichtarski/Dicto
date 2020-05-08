670a4d5531b9550c5718b00e3cd0ba02d30796ce
rradzzio <32550649+rradzzio@users.noreply.github.com>
2020-05-08 15:04:39 +0200

    Test WordsApi in MainActivity - add headers inside an AppModule

--- app/src/main/java/com/example/toja/dicto/MainActivity.java
+++ app/src/main/java/com/example/toja/dicto/MainActivity.java
@@ -1,19 +1,68 @@
 package com.example.toja.dicto;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
+
+import com.example.toja.dicto.models.Translation;
+import com.example.toja.dicto.network.WordsApi;
+import com.example.toja.dicto.utils.Resource;
+
+import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends DaggerAppCompatActivity {
 
     private static final String TAG = "MainActivity";
+
+    @Inject
+    WordsApi wordsApi;
+
+    private MediatorLiveData<Resource<Translation>> translation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        checkApi().observe(this,translationResource -> {
+            if (translationResource != null)
+                switch (translationResource.status) {
+                    case SUCCESS: {
+                        Log.d(TAG,"onChanged: " + translationResource.data.getWord());
+                    }
+                }
+        });
+    }
 
+    LiveData<Resource<Translation>> checkApi() {
+        if(translation == null) {
+            translation = new MediatorLiveData<>();
+            translation.setValue(Resource.loading(null));
+        }
+
+        LiveData<Resource<Translation>> source = LiveDataReactiveStreams.fromPublisher(wordsApi.getWordTranslation("car")
+                .map((Function<Translation, Resource<Translation>>) translation -> {
+                    if(translation != null) {
+                        return Resource.success(translation);
+                    }
+                    return Resource.error("Something went wrong", null);
+                })
+                .subscribeOn(Schedulers.io())
+        );
+        translation.addSource(source,translationResource -> {
+            translation.setValue(translationResource);
+            translation.removeSource(source);
+        });
+
+        return translation;
     }
 
 }
--- app/src/main/java/com/example/toja/dicto/di/AppModule.java
+++ app/src/main/java/com/example/toja/dicto/di/AppModule.java
@@ -1,14 +1,29 @@
 package com.example.toja.dicto.di;
 
+import com.example.toja.dicto.network.WordsApi;
+import com.example.toja.dicto.utils.Keys;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 import static com.example.toja.dicto.utils.Constants.WORDS_API_BASE_URL;
+import static com.example.toja.dicto.utils.Constants.X_RAPIDAPI_HOST;
+import static com.example.toja.dicto.utils.Constants.X_RAPIDAPI_HOST_VALUE;
+import static com.example.toja.dicto.utils.Constants.X_RAPIDAPI_KEY;
 
 @Module
 public abstract class AppModule {
@@ -17,12 +32,36 @@ public abstract class AppModule {
 
     @Singleton
     @Provides
-    private static Retrofit provideRetrofitInstance() {
+    static Retrofit provideRetrofitInstance() {
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(chain -> {
+                    Request originalRequest = chain.request();
+
+                    Request newRequest = originalRequest.newBuilder()
+                            .header(X_RAPIDAPI_HOST, X_RAPIDAPI_HOST_VALUE)
+                            .header(X_RAPIDAPI_KEY,Keys.INSTANCE.apiKey())
+                            .build();
+                    return chain.proceed(newRequest);
+                })
+                .addInterceptor(loggingInterceptor)
+                .build();
+
         return new Retrofit.Builder()
                 .baseUrl(WORDS_API_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(okHttpClient)
                 .build();
+    }
+
+    @Singleton
+    @Provides
+    static WordsApi provideWordsApi(Retrofit retrofit) {
+        return retrofit.create(WordsApi.class);
     }
 
 }
