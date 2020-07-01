73b2d8bbdb3889bf539da2611133fc7e3bf4bb38
rradzzio <32550649+rradzzio@users.noreply.github.com>
2020-07-01 12:25:50 +0200

    Basic navigation drawer setup

--- app/src/main/java/com/example/toja/dicto/MainActivity.java
+++ app/src/main/java/com/example/toja/dicto/MainActivity.java
@@ -1,20 +1,32 @@
 package com.example.toja.dicto;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.toja.dicto.ui.main.history.HistoryFragment;
 import com.example.toja.dicto.ui.main.translation.TranslationFragment;
+import com.google.android.material.navigation.NavigationView;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
-public class MainActivity extends DaggerAppCompatActivity {
+public class MainActivity extends DaggerAppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private static final String TAG = "MainActivity";
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
 
         testTranslationFragment();
     }
@@ -25,4 +37,24 @@ public class MainActivity extends DaggerAppCompatActivity {
                 .commit();
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+
+            case R.id.nav_translate:{
+
+
+                break;
+            }
+
+            case R.id.nav_history:{
+
+                break;
+            }
+        }
+        item.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
 }
